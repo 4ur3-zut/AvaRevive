0997a5c074a5913310863d42ba69e3f29e71f9c8
4ur3-zut <->
2022-01-23 17:08:30 +0100

    optimize fetches, fix some edge cases, add drop shadow

--- avaRevive.mjs
+++ avaRevive.mjs
@@ -5,51 +5,21 @@ import localforage from 'localforage'
 const avatarDimensions = {
   forum: 80,
   nexus: 48,
-  liker: 24
+  liker: 24,
+  notifAndMenuTabs: 20
 }
+
+const style = document.createElement('style')
+style.id = 'avaReviveStyle'
+style.innerText = `
+.revived { filter: drop-shadow(0 0 2px black); }`
+document.body.append(style)
+
+const ongoingFetches = {}
 
 localforage.getItem('knownAvatars').then(
   (knownAvatars) => {
     if (!knownAvatars) knownAvatars = {}
-
-    const style = document.createElement('style')
-    style.id = 'avaReviveStyle'
-    style.innerText = '.tid_content.tid_modinit { display: none!important; }'
-
-    /**
-     * create a new observer
-     *
-     * @param {Element} element the element that needs to be observed
-     */
-    const createObserver = element => {
-      const o = new MutationObserver(async () => {
-        const posts = [
-          ...document.getElementsByClassName('tid_comment')
-        ]
-        for (const post of posts) {
-          const slow = []
-          const fast = []
-          if (!post.classList.contains('parsed')) {
-            post.classList.add('parsed')
-
-            const userElem = post.getElementsByClassName('tid_user')[0]
-
-            if (!userElem) continue // <- continue gracefully if data is not found
-
-            const user = userElem.getAttribute('tid_id')
-            if (user in knownAvatars) fast.push([post, user]) // do fast if possible
-            slow.push([post, user]) // do slows anyways to check if url changed
-
-            for (const [p, u] of fast) reviveFast(p, u)
-            for (const [p, u] of slow) revive(p, u)
-          }
-        }
-      })
-      o.observe(
-        element,
-        { childList: true }
-      )
-    }
 
     /**
      * find a post's avatar
@@ -58,6 +28,20 @@ localforage.getItem('knownAvatars').then(
      * @returns {string | null} url if a new url is found
      */
     const findAvatar = async (user) => {
+      // if the avatar is already being fetched:
+      if (ongoingFetches[user]) return await ongoingFetches[user]
+
+      // register ongoing fetch:
+      let resolveP
+      ongoingFetches[user] = new Promise((resolve) => {
+        resolveP = resolve
+        setTimeout(() => { // de-register after 30sec
+          if (!ongoingFetches[user].resolved) resolve(null) // <- resolve to null if somehow not resolved
+          ongoingFetches[user] = undefined
+        }, 30000)
+      })
+
+      // fetch:
       const profileRequest = await fetch(`https://twinoid.com/user/${user}`)
       const profileText = await profileRequest.text()
       const profilePage = document.createElement('html')
@@ -68,11 +52,14 @@ localforage.getItem('knownAvatars').then(
 
       if (url) {
         if (url !== knownAvatars[user]) knownAvatars[user] = url
-        else return null // return null if did not change
+        else {
+          resolveP(null)
+          return null // return null if did not change
+        }
       }
 
       localforage.setItem('knownAvatars', knownAvatars)
-
+      resolveP(url)
       return url
     }
 
@@ -143,6 +130,8 @@ localforage.getItem('knownAvatars').then(
         containerType = 'liker'
       } else {
         containerType = 'nexus'
+        // check for special cases
+        if (post.getElementsByClassName('tid_twinoidAvatar')[0].style.maxHeight === '20px') containerType = 'notifAndMenuTabs'
       }
 
       let avaContainer
@@ -170,6 +159,7 @@ localforage.getItem('knownAvatars').then(
       const dims = avatarDimensions[containerType]
       revived.style.maxWidth = dims + 'px'
       revived.style.maxHeight = dims + 'px'
+      if (containerType === 'forum') revived.classList.add('revived')
 
       while (avaContainer.children.length) avaContainer.lastChild.remove()
       avaContainer.append(revived)
@@ -183,8 +173,11 @@ localforage.getItem('knownAvatars').then(
       const o = new MutationObserver(async () => {
         const mainAvatarEl = document.querySelector('.tid_actions a') // avatar inside notification panel
         if (!mainAvatarEl) return
+        const loggedUser = mainAvatarEl.href.split('/')[4]
 
         o.disconnect()
+
+        // Handle normal posts:
         const posts = [
           ...document.querySelectorAll('[id^=tid_forumPost]'), // forums posts
           ...document.querySelectorAll('.tid_wallEvent:not(.tid_likeOnly)'), // nexus posts
@@ -192,50 +185,48 @@ localforage.getItem('knownAvatars').then(
           ...document.getElementsByClassName('tid_liker') // nexus likes
         ]
         for (const post of posts) {
-          const slow = []
-          const fast = []
           if (!post.classList.contains('parsed')) {
             post.classList.add('parsed')
 
             const userElem = post.getElementsByClassName('tid_user')[0]
 
             const isLiker = post.classList.contains('tid_liker')
-            if (!userElem && !isLiker) continue // <- continue gracefully if data is not found
+            if (!userElem && !isLiker) {
+              if (post.classList.contains('tid_comment')) {
+                // assume that an ID-less comment is a logged user comment
+                if (loggedUser in knownAvatars) reviveFast(post, loggedUser)
+                revive(post, loggedUser)
+              }
+              continue // <- continue gracefully if data is not found
+            }
 
-            const user = isLiker ? post.getAttribute('tid_id') : userElem.getAttribute('tid_id')
-            if (user in knownAvatars) fast.push([post, user]) // do fast if possible
-            slow.push([post, user]) // do slows anyways to check if url changed
-
-            for (const [p, u] of fast) reviveFast(p, u)
-            for (const [p, u] of slow) revive(p, u)
-          }
-
-          if (post.classList.contains('tid_wallEvent')) {
-            createObserver(post.children[1])
+            const user = isLiker
+              ? post.getAttribute('tid_id')
+              : userElem.getAttribute('tid_id')
+            if (user in knownAvatars) reviveFast(post, user) // do fast if possible
+            revive(post, user) // do slow anyways to check if url changed
           }
         }
 
-        const newNexusPost = document.getElementsByClassName('tid_newPost')
-
-        const elements = [
+        // Handle the current user's avatars in special places:
+        const userAvatars = [
           document.querySelectorAll('ul.menu li')[7], // <- "my profile" tab
           document.getElementById('tid_openRight'), // <- top right notification area
           mainAvatarEl
         ]
+        const newNexusPost = document.getElementsByClassName('tid_newPost')
         if (newNexusPost.length) {
-          elements.push(newNexusPost[0])
+          userAvatars.push(newNexusPost[0])
         }
-
-        for (const element of elements) {
+        for (const element of userAvatars) {
           if (!element.classList.contains('parsed')) {
             element.classList.add('parsed')
-
-            const user = mainAvatarEl.href.split('/')[4]
-            if (user in knownAvatars) reviveFast(element, user) // do fast if possible
-            revive(element, user) // do slows anyways to check if url changed
+            if (loggedUser in knownAvatars) reviveFast(element, loggedUser) // do fast if possible
+            revive(element, loggedUser) // do slow anyways to check if url changed
           }
         }
 
+        // reconnect observer:
         o.observe(
           document.getElementById('tid_forum_right') || document.getElementsByClassName('tid_wallEvents')[0] || document.getElementsByClassName('tid_module')[0],
           { childList: true }
